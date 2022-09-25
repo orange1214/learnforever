6c0374d55e5858029f8410690acdbf55dd043944
Ram Osuri <osurir@gmail.com>
2022-09-25 18:30:38 -0400

    new changes for the pipeline

--- main.tf
+++ main.tf
@@ -12,3 +12,25 @@ provider "aws" {
   secret_key = var.aws_secret_key
   region     = var.aws_region
 }
+
+resource "aws_instance" "test" {
+  instance_type = var.instance_type
+  ami = data.aws_ami.ubuntu.id
+  
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["431404720664"] # Canonical
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable aws_region {}
+
+variable "instance_type" {
+  type        = string
+  description = "Type for EC2 Instnace"
+  default     = "t2.micro"
+}
